f25543e8bf507a4f22bfd7d5f2f96616534a2fa6
Xybronix <xybronix@gmail.com>
2026-01-26 10:06:18 +0100

    Garder uniquement le code backend

--- mobile/services/index.ts
+++
@@ -1,7 +0,0 @@
-export { authService, type User, type LoginCredentials, type RegisterData } from './authService';
-export { userService, type UserStats, type UpdateProfileData } from './userService';
-export { bikeService, type Bike, type Location } from './bikeService';
-export { rideService, type Ride, type RideStats } from './rideService';
-export { walletService, type WalletBalance, type Transaction, type DepositRequest } from './walletService';
-export { chatService } from './chatService';
-export { notificationService, type Notification, type NotificationPreferences } from './notificationService';
